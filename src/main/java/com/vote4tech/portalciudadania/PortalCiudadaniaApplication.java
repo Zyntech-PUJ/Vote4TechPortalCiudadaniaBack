8a59a8b4aa95d2a557a8e4acbf791d52f8b2855b
Jeisson177 <79467624+Jeisson177@users.noreply.github.com>
2026-04-26 16:10:34 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/vote4tech/portalciudadania/PortalCiudadaniaApplication.java
@@ -0,0 +1,12 @@
+package com.vote4tech.portalciudadania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalCiudadaniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalCiudadaniaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/vote4tech/portalciudadania/PortalCiudadaniaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.vote4tech.portalciudadania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalCiudadaniaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
